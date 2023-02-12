b773a7f01ceda13f872f65ba3c7315781c860f37
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-02-12 11:27:54 +0100

    Colores y traducciones

--- src/main/java/com/parkingmanager/parkingmanagerweb/user/adapter/UserController.java
+++ src/main/java/com/parkingmanager/parkingmanagerweb/user/adapter/UserController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import com.parkingmanager.parkingmanagerweb.user.domain.User;
 import com.parkingmanager.parkingmanagerweb.user.service.*;;
 @Controller
 public class UserController {
+    private final Log log = LogFactory.getLog(getClass());
 
     private UserService userService;
     public UserController(@Autowired UserService userService) {
@@ -18,7 +20,7 @@ public class UserController {
 
     @GetMapping("/users")
     public String showAllUsers(Model model){
-
+        log.info("Devolviendo usuarios");
         Iterable<User> allUsers = userService.readAll();
 
         model.addAttribute("users",allUsers);
