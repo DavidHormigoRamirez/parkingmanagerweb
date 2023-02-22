e8080641bc8308e2af1d24df6d40355f6f437989
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-02-22 11:26:49 +0100

    Cambios test unitari

--- src/test/java/com/parkingmanager/parkingmanagerweb/user/UserUnitTests.java
+++ src/test/java/com/parkingmanager/parkingmanagerweb/user/UserUnitTests.java
@@ -1,4 +1,4 @@
-package com.parkingmanager.parkingmanagerweb;
+package com.parkingmanager.parkingmanagerweb.user;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
